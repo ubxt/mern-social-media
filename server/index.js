7c8fd73c325c75d9e34dd2dd9cc0e16cc34af239
ubxt <ubtofficial@hotmail.com>
2023-01-25 01:36:51 +0300

    first part of the course

---
+++ server/index.js
@@ -0,0 +1,57 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import authRoutes from "./routes/auth.js";
+import { fileURLToPath } from "url";
+
+/* CONFIG */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+import { register } from "./controllers/auth.js";
+
+/* FILE STORAGE */
+const storage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		cb(null, "public/assets");
+	},
+	filename: (req, file, cb) => {
+		cb(null, file.originalname);
+	},
+});
+const upload = multer({ storage });
+
+/* ROUTES WITH FILES */
+app.post("/auth/register", upload.single("picture"), register);
+
+/* ROUTES */
+app.use("/auth", authRoutes);
+
+/* MONGOOSE SETUP */
+// deprecation warn
+mongoose.set("strictQuery", false);
+const PORT = process.env.PORT || 6001;
+mongoose
+	.connect(process.env.MONGO_URL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => {
+		app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+	})
+	.catch((error) => console.error(`${error} did not connect`));

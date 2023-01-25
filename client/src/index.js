9f77c612d59607923e60bb25ab610f0750bcf7a0
ubxt <ubtofficial@hotmail.com>
2023-01-26 00:59:09 +0300

    added theme, some bugfixes, state configs

--- client/src/index.js
+++ client/src/index.js
@@ -1,22 +1,22 @@
+import { configureStore } from "@reduxjs/toolkit";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import authReducer from "./state";
-import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 import {
-	persistStore,
-	persistReducer,
 	FLUSH,
-	REHYDRATE,
 	PAUSE,
 	PERSIST,
 	PURGE,
 	REGISTER,
+	REHYDRATE,
+	persistReducer,
+	persistStore,
 } from "redux-persist";
-import storage from "redux-persist/lib/storage";
 import { PersistGate } from "redux-persist/integration/react";
+import storage from "redux-persist/lib/storage";
+import App from "./App";
+import "./index.css";
+import authReducer from "./state";
 
 const persistConfig = { key: "root", storage, version: 1 };
 const persistedReducer = persistReducer(persistConfig, authReducer);
--- client/src/state/index.js
+++ client/src/state/index.js
@@ -34,7 +34,7 @@ export const authSlice = createSlice({
 		},
 		setPost: (state, action) => {
 			const updatedPosts = state.posts.map((post) => {
-				if (post._id === action.payload.post_id)
+				if (post._id === action.payload.post._id)
 					return action.payload.post;
 				return post;
 			});
@@ -45,4 +45,4 @@ export const authSlice = createSlice({
 
 export const { setMode, setLogin, setLogout, setFriends, setPosts, setPost } =
 	authSlice.actions;
-export default authSlice;
+export default authSlice.reducer;

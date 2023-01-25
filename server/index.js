0e91a99e8df303c76cafafe583ccc705328a22a5
ubxt <ubtofficial@hotmail.com>
2023-01-25 22:43:21 +0300

    User routes

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
 import { fileURLToPath } from "url";
 
 /* CONFIG */
@@ -41,6 +42,7 @@ app.post("/auth/register", upload.single("picture"), register);
 
 /* ROUTES */
 app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
 
 /* MONGOOSE SETUP */
 // deprecation warn
